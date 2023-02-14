34b56846f82c55fc4b085ea037a374e8860eb788
xuanyuan <xuanyuanshenfu@outlook.com>
2023-02-14 10:42:58 +0800

    init

---
+++ Sources/include/MyPoint.c
@@ -0,0 +1,8 @@
+//
+//  MyPoint.c
+//  
+//
+//  Created by xuanyuan on 2023/2/14.
+//
+
+#include <stdio.h>
---
+++ Sources/include/MyPoint.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  
+//
+//  Created by xuanyuan on 2023/2/14.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
+
+struct MyPoint {
+   int x;
+   int y;
+};
